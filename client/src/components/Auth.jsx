5fcbd3e4cd8d93637c05b53369c1c366ad00f3d3
Maroob123 <syedmuhammadmaroob123@gmail.com>
2022-11-01 04:50:37 +0500

    deployment bug fixes

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -30,7 +30,7 @@ const Auth = () => {
         setLoading(true);
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://fnf-chat.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
